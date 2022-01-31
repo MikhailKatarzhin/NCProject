ed60b9a5bb25a183da0fae1fffa291978a25f41f
MikhailKatarzhin <58183630+MikhailKatarzhin@users.noreply.github.com>
2022-01-31 22:41:11 +0400

    removed unused imports

--- src/main/java/ncp/service/UserService.java
+++ src/main/java/ncp/service/UserService.java
@@ -1,17 +1,12 @@
 package ncp.service;
 
-import ncp.model.Role;
 import ncp.model.User;
 import ncp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Service
 public class UserService implements UserDetailsService {
