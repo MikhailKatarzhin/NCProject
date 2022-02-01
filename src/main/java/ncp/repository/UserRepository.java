137462ef927c68dbab2589eafbcd3dbcdc7bdcbc
MikhailKatarzhin <58183630+MikhailKatarzhin@users.noreply.github.com>
2022-02-01 18:46:58 +0400

    added query for compute count users by email

--- src/main/java/ncp/repository/UserRepository.java
+++ src/main/java/ncp/repository/UserRepository.java
@@ -2,9 +2,17 @@ package ncp.repository;
 
 import ncp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     User findByUsername(String username);
+
+    @Query(
+            value = "select count(*) from user where email = ?",
+            nativeQuery = true
+    )
+    Long countByEmail(String email);
 }
