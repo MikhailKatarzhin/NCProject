9dc9b127861125c9c14e2137ad1e27ac9b75cdca
MikhailKatarzhin <58183630+MikhailKatarzhin@users.noreply.github.com>
2022-02-02 20:46:25 +0400

    country, city, house - mandatory now, descriptions for another field

--- src/main/java/ncp/model/Address.java
+++ src/main/java/ncp/model/Address.java
@@ -14,17 +14,33 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String country;
 
+    /**
+     * null - there is no separation into regions
+     * As example Vatican
+     */
     private String region;
 
+    @Column(nullable = false)
     private String city;
 
+    /**
+     * null - there is no separation into streets
+     */
     private String street;
 
+    @Column(nullable = false)
     private Long house;
 
+    /**
+     * null - there is no separation into building
+     */
     private Long building;
 
+    /**
+     * null - there is no separation into flats
+     */
     private Long flat;
 }
