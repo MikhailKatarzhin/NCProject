82612a53c0db2f9312650c50e4d8dc1efc8d0c78
MikhailKatarzhin <58183630+MikhailKatarzhin@users.noreply.github.com>
2022-02-02 20:59:44 +0400

    init AdministrationController and html for it

---
+++ src/main/java/ncp/controller/AdministrationController.java
@@ -0,0 +1,16 @@
+package ncp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/administration")
+public class AdministrationController {
+
+    @GetMapping
+    public String administration(){
+        return "administration";
+    }
+
+}
