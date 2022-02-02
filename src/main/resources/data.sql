47a44456bba99b72c31ee756ffdeffe44cc30ee4
MikhailKatarzhin <58183630+MikhailKatarzhin@users.noreply.github.com>
2022-02-02 20:40:06 +0400

    personality requires id, user not requires id personality, add insert test address

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,13 +10,15 @@ insert into transmitter_status (name) values('turned on');
 insert into transmitter_status (name) values('damaged');
 insert into transmitter_status (name) values('being repaired');
 
-INSERT INTO personality (first_name, last_name, patronymic) VALUES ('','','');
-INSERT INTO user (email, password, username, personality_id)
-VALUES ('maikl.1997@mail.ru', '$2a$10$O9gJ0LzfKTwnV.udAG3oHOqtE5YXElvz.aSDKOFY39Jfc9ls5DfqW', 'admin', 1);
+INSERT INTO personality (id, first_name, last_name, patronymic) VALUES (1,'','','');
+INSERT INTO user (email, password, username)
+VALUES ('maikl.1997@mail.ru', '$2a$10$O9gJ0LzfKTwnV.udAG3oHOqtE5YXElvz.aSDKOFY39Jfc9ls5DfqW', 'admin');
 INSERT INTO user_role_set(user_id, role_set_id) VALUES (1, 3);
 
-INSERT INTO personality (first_name, last_name, patronymic) VALUES ('Тестер','Тестеров','Тестирович');
-INSERT INTO user (email, password, username, personality_id)
-VALUES ('tester@mail.ru', '$2a$10$O9gJ0LzfKTwnV.udAG3oHOqtE5YXElvz.aSDKOFY39Jfc9ls5DfqW', 'tester', 2);
+INSERT INTO personality (id,first_name, last_name, patronymic) VALUES (2,'Тестер','Тестеров','Тестирович');
+INSERT INTO user (email, password, username)
+VALUES ('tester@mail.ru', '$2a$10$O9gJ0LzfKTwnV.udAG3oHOqtE5YXElvz.aSDKOFY39Jfc9ls5DfqW', 'tester');
 INSERT INTO user_role_set(user_id, role_set_id) VALUES (2, 1);
 INSERT INTO user_role_set(user_id, role_set_id) VALUES (2, 2);
+
+INSERT INTO address(id, building, city, country, flat, house, region, street) VALUES (1, 1, 'test', 'test', 1, 1, 'test', 'test');
