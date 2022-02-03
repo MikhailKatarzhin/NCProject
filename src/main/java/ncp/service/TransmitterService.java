1b8bd73b53618c6458fd119e58b7c04f54387931
MikhailKatarzhin <58183630+MikhailKatarzhin@users.noreply.github.com>
2022-02-03 14:58:57 +0400

    TransmitterService and TransmitterStatusService init

---
+++ src/main/java/ncp/service/TransmitterService.java
@@ -0,0 +1,51 @@
+package ncp.service;
+
+import ncp.model.Transmitter;
+import ncp.repository.TransmitterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static ncp.config.ProjectConstants.ROW_COUNT;
+
+@Service
+public class TransmitterService {
+    @Autowired
+    private TransmitterRepository transmitterRepository;
+    @Autowired
+    private TransmitterStatusService transmitterStatusService;
+
+    public long pageCount(){
+        long nPage=transmitterRepository.count()/ROW_COUNT + (transmitterRepository.count()%ROW_COUNT == 0? 0:1);
+        return nPage == 0? nPage+1 : nPage;
+    }
+
+    public List<Transmitter> transmitterListByNumberPageList(long numberPageList){
+        return transmitterRepository.selectByLimitOffset(ROW_COUNT, (numberPageList-1)*ROW_COUNT);
+    }
+
+    public Transmitter saveNew(String description){
+        Transmitter transmitter = new Transmitter();
+        transmitter.setDescription(description);
+        transmitter.setStatus(transmitterStatusService.getById(1));
+        transmitter.setAvailableAddresses(new HashSet<>());
+        return transmitterRepository.save(transmitter);
+    }
+
+    public Transmitter save(Transmitter transmitter){
+        return transmitterRepository.save(transmitter);
+    }
+    public Transmitter setStatus(long transmitterId, long statusId){
+        Transmitter transmitter = transmitterRepository.getById(transmitterId);
+        transmitter.setStatus(transmitterStatusService.getById(statusId));
+        return transmitterRepository.save(transmitter);
+    }
+    public Transmitter getById(long id){
+        return transmitterRepository.getById(id);
+    }
+    public void deleteById(long id){
+        transmitterRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/ncp/service/TransmitterStatusService.java
@@ -0,0 +1,21 @@
+package ncp.service;
+
+import ncp.model.TransmitterStatus;
+import ncp.repository.TransmitterStatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TransmitterStatusService {
+    @Autowired
+    private TransmitterStatusRepository transmitterStatusRepository;
+
+    public TransmitterStatus getById(long id){
+        return transmitterStatusRepository.getById(id);
+    }
+    public List<TransmitterStatus> getAll(){
+        return transmitterStatusRepository.findAll();
+    }
+}
