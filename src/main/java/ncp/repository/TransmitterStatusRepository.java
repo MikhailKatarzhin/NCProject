5df5eeafa3d226df058a27fd1b0dea9e94dc52a9
MikhailKatarzhin <58183630+MikhailKatarzhin@users.noreply.github.com>
2022-02-03 17:29:03 +0400

    add query TransmitterStatus by status name

--- src/main/java/ncp/repository/TransmitterStatusRepository.java
+++ src/main/java/ncp/repository/TransmitterStatusRepository.java
@@ -2,8 +2,14 @@ package ncp.repository;
 
 import ncp.model.TransmitterStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransmitterStatusRepository extends JpaRepository<TransmitterStatus, Long> {
+    @Query(
+            value = "SELECT * FROM transmitter_status WHERE name =?"
+            , nativeQuery = true
+    )
+    TransmitterStatus getByStatusName(String statusName);
 }
