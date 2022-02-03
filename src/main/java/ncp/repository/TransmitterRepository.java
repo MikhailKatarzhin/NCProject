d12b841cafdb0eef445b50f0d24959bd9d6c9b41
MikhailKatarzhin <58183630+MikhailKatarzhin@users.noreply.github.com>
2022-02-03 14:58:14 +0400

    add query for list Transmitters by limit and offset

--- src/main/java/ncp/repository/TransmitterRepository.java
+++ src/main/java/ncp/repository/TransmitterRepository.java
@@ -2,8 +2,16 @@ package ncp.repository;
 
 import ncp.model.Transmitter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TransmitterRepository extends JpaRepository<Transmitter, Long> {
+    @Query(
+            value = "SELECT * FROM transmitter ORDER BY id LIMIT ?1 OFFSET ?2"
+            , nativeQuery = true
+    )
+    List<Transmitter> selectByLimitOffset(Long limit, Long offset);
 }
