da2761fdba34a675d9f05f4f139b6ef11bb4d8d5
MikhailKatarzhin <58183630+MikhailKatarzhin@users.noreply.github.com>
2022-02-05 13:31:49 +0400

    add TariffStatusRepository and TariffStatusService

--- src/main/java/ncp/repository/TariffStatusRepository.java
+++ src/main/java/ncp/repository/TariffStatusRepository.java
@@ -2,10 +2,15 @@ package ncp.repository;
 
 import ncp.model.TariffStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TariffStatusRepository extends JpaRepository<TariffStatus, Long> {
-
+    @Query(
+            value = "SELECT * FROM tariff_status WHERE name =?"
+            , nativeQuery = true
+    )
+    TariffStatus getByName(String name);
 
 }
---
+++ src/main/java/ncp/service/TariffStatusService.java
@@ -0,0 +1,24 @@
+package ncp.service;
+
+import ncp.model.TariffStatus;
+import ncp.repository.TariffStatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TariffStatusService {
+    @Autowired
+    TariffStatusRepository tariffStatusRepository;
+
+    public TariffStatus getByName(String name){
+        return tariffStatusRepository.getByName(name);
+    }
+    public TariffStatus getById(long id){
+        return tariffStatusRepository.getById(id);
+    }
+    public List<TariffStatus> getAll(){
+        return tariffStatusRepository.findAll();
+    }
+}
