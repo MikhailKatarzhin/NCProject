02d78fbe207d82ce1bc3c24d8a41905e037a6e15
MikhailKatarzhin <58183630+MikhailKatarzhin@users.noreply.github.com>
2022-02-06 23:13:21 +0400

    feat: add ContractController

---
+++ src/main/java/ncp/controller/ContractController.java
@@ -0,0 +1,128 @@
+package ncp.controller;
+
+import ncp.controller.paging.AbstractSecondaryPagingController;
+import ncp.model.Address;
+import ncp.model.Contract;
+import ncp.model.Tariff;
+import ncp.service.implementations.ContractServiceImp;
+import ncp.service.implementations.TariffService;
+import ncp.service.implementations.UserService;
+import ncp.service.interfaces.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/contract")
+public class ContractController extends AbstractSecondaryPagingController {
+
+    private final ContractServiceImp contractService;
+    private final AddressService addressService;
+    private final TariffService tariffService;
+    private final UserService userService;
+
+    @Autowired
+    public ContractController(ContractServiceImp contractService, TariffService tariffService
+            , UserService userService, AddressService addressService){
+        this.contractService = contractService;
+        this.addressService = addressService;
+        this.tariffService = tariffService;
+        this.userService = userService;
+    }
+
+///********************! Contract management !********************
+
+    @GetMapping
+    public String management(){
+        return firstPage();
+    }
+
+    @GetMapping("/list/{currentPage}")
+    public String managementByOffset(@PathVariable Long currentPage, ModelMap model){
+        if (currentPage < 1L)
+            return firstPage();
+        long nPage=contractService.pageCount(userService.getRemoteUserId());
+        if (currentPage > nPage)
+            return lastPage();
+        model.addAttribute("nPage", nPage);
+        model.addAttribute("currentPage", currentPage);
+        List<Contract> contractList =contractService.contractListByConsumerIdAndNumberPageList(
+                userService.getRemoteUserId(), currentPage);
+        model.addAttribute("contracts", contractList);
+        return "contract/management";
+    }
+
+    @PostMapping("/list/{currentPage}/{id}/remove")
+    public String removeById(@PathVariable Long currentPage, @PathVariable Long id){
+        contractService.terminateContractById(id);
+        return "redirect:/address/list/"+currentPage;
+    }
+
+    @PostMapping("/list/{currentPage}/signContract/{tariffId}")
+    public String signContract(@PathVariable Long tariffId, @RequestParam Long addressId, @PathVariable Long currentPage){
+        Address address = new Address();
+        contractService.signContract(
+                tariffService.getById(tariffId), userService.getRemoteUser(), address);
+        return firstPage();
+    }
+
+///********************! Pagination Contract !********************
+
+    @Override
+    protected Long primaryPageCount() {
+        return contractService.pageCount(userService.getRemoteUserId());
+    }
+
+    @Override
+    protected String getPrimaryPrefix() {
+        return "/contract";
+    }
+
+///********************! Tariff searching !********************
+
+    @GetMapping("/select_address")
+    public String selectAddress(ModelMap model){
+        return selectAddress(new Address(), model);
+    }
+
+    @GetMapping("/select_address/list")
+    public String selectAddress(Address address, ModelMap model){
+        if (address==null)
+            address = new Address();
+        model.addAttribute("searchAddress", address);
+        model.addAttribute("addresses", addressService.searchAddressLikeAddress(address, 1L));
+        return "contract/select_address";
+    }
+
+    @GetMapping("/select_tariff/{addressId}/list")
+    public String selectTariff(@PathVariable Long addressId, ModelMap model){
+        return selectTariff(addressId, 1L,new Tariff(), model);
+    }
+
+    @GetMapping("/select_tariff/{addressId}/list/{currentPage}")
+    public String selectTariff(@PathVariable Long addressId, @PathVariable Long currentPage
+            , Tariff tariff, ModelMap model){
+        if (tariff==null)
+            tariff = new Tariff();
+        model.addAttribute("searchTariff", tariff);
+        model.addAttribute("tariffs",
+                tariffService.selectByLimitOffsetAndAvailableAddressIdAndTurnedOnTransmitterAndActiveTariff(
+                        currentPage, addressId));
+        return "contract/select_tariff";
+    }
+
+///********************! Pagination Tariff !********************
+
+    @Override
+    protected Long secondaryPageCount(Long id) {
+        return tariffService.countByAvailableAddressIdAndTurnedOnTransmitterAndActiveTariff(id);
+    }
+
+    @Override
+    protected String getSecondaryPrefix() {
+        return "/select_tariff";
+    }
+}
