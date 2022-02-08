02a84c5f46fab6a92be8bcd07dcd8c7936fe81c7
MikhailKatarzhin <58183630+MikhailKatarzhin@users.noreply.github.com>
2022-02-08 23:49:34 +0400

    feat: user can replenishment his balance from 50 to 1mln

--- src/main/java/ncp/controller/WalletController.java
+++ src/main/java/ncp/controller/WalletController.java
@@ -4,7 +4,11 @@ import ncp.service.interfaces.UserService;
 import ncp.service.interfaces.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/wallet")
@@ -19,4 +23,18 @@ public class WalletController {
         this.userService = userService;
     }
 
+    @GetMapping("/replenishment")
+    public String replenishment(ModelMap model){
+        model.addAttribute("balance", walletService.getBalance(userService.getRemoteUserId()));
+        return "wallet/replenishment";
+    }
+
+    @PostMapping("/replenishment")
+    public String replenishment(@RequestParam Long incomeFounds, ModelMap model){
+        if (walletService.replenishmentFunds(incomeFounds, userService.getRemoteUserId())) {
+            return "redirect:/profile";
+        }
+        model.addAttribute("replenishmentFailed", "Replenishment failed");
+        return replenishment(model);
+    }
 }
