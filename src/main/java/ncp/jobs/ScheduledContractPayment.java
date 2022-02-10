04498bb75785aba6abfc741055a9e3d065e9bab2
MikhailKatarzhin <58183630+MikhailKatarzhin@users.noreply.github.com>
2022-02-10 22:53:22 +0400

    feat: logging start, end and statistics of scheduled jobs

--- src/main/java/ncp/jobs/ScheduledContractPayment.java
+++ src/main/java/ncp/jobs/ScheduledContractPayment.java
@@ -5,6 +5,8 @@ import ncp.model.User;
 import ncp.model.Wallet;
 import ncp.repository.ContractRepository;
 import ncp.repository.WalletRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,8 @@ import java.util.List;
 
 @Service
 public class ScheduledContractPayment {
+
+    private final static Logger logger = LoggerFactory.getLogger(ScheduledContractPayment.class);
 
     private final Long rowInPage = 100L;
     private final ContractRepository contractRepository;
@@ -26,7 +30,12 @@ public class ScheduledContractPayment {
 
     @Scheduled(cron = "0 0 12 * * *")
     public void contractsPayment() {
+        logger.trace("Scheduled payment of contracts has started");
+
         Long nRow = contractRepository.countExpiredWithNonInactiveTariff();
+        logger.debug("Expired contracts quantity: {}", nRow);
+
+        Long confirmedPayment = 0L;
         Long nPage = nRow / rowInPage + (nRow % rowInPage == 0L ? 0L : 1L);
         for (Long i = nPage; i > 0; i--) {
             List<Contract> contractList = contractRepository.selectExpiredWithNonInactiveTariffByLimitOffset(
@@ -40,8 +49,11 @@ public class ScheduledContractPayment {
                     Wallet providerWallet = contract.getTariff().getProvider().getWallet();
                     providerWallet.replenishmentFunds(contract.getTariff().getPrice());
                     walletRepository.save(providerWallet);
+                    confirmedPayment++;
                 }
             }
         }
+        logger.info("Scheduled payment of contracts has ended. Confirmed payments: {}. Failed payments: {}"
+                , confirmedPayment, nRow - confirmedPayment);
     }
 }
--- src/main/java/ncp/jobs/ScheduledTariffPayment.java
+++ src/main/java/ncp/jobs/ScheduledTariffPayment.java
@@ -2,6 +2,8 @@ package ncp.jobs;
 
 import ncp.model.*;
 import ncp.repository.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,8 @@ import java.util.List;
 
 @Service
 public class ScheduledTariffPayment {
+
+    private final static Logger logger = LoggerFactory.getLogger(ScheduledTariffPayment.class);
 
     private final Long rowInPage = 100L;
 
@@ -27,7 +31,12 @@ public class ScheduledTariffPayment {
 
     @Scheduled(cron = "0 0 0 * * *")
     public void contractsPayment() {
+        logger.trace("Scheduled payment of tariffs has started");
+
         Long nRow = tariffRepository.countNonInactive();
+        logger.debug("Tariffs quantity: {}", nRow);
+
+        Long inactivatedTariffs = 0L;
         Long nPage = nRow / rowInPage + (nRow % rowInPage == 0L ? 0L : 1L);
         for (Long i = nPage; i > 0; i--) {
             List<Tariff> tariffList = tariffRepository.selectNonInactiveByLimitOffset(
@@ -41,8 +50,11 @@ public class ScheduledTariffPayment {
                 } else {
                     tariff.setStatus(tariffStatusRepository.getByName("inactive"));
                     tariffRepository.save(tariff);
+                    inactivatedTariffs++;
                 }
             }
         }
+        logger.info("Scheduled payment of tariffs has ended. Confirmed payments: {}. Inactivated tariffs: {}"
+                , nRow - inactivatedTariffs, inactivatedTariffs);
     }
 }
